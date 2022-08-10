e25a0f2a3f0bfb6f808411918b0b24c3d6713594
kyondoku <kyondoku@gmail.com>
2022-08-11 01:10:05 +0900

    header 작업중

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,10 +4,14 @@ import styles from "./header.module.css";
 const Header = () => (
     <header className={styles.header}>
         <div className={styles.logo}>
-        {/* <img className={styles.img} src="/images/logo.png" alt="logo" /> */}
-        <h1 className={styles.title}>MOS</h1>
+            <img className={styles.img} src="/images/logo.png" alt="logo" />
         </div>
         {/* <img className={styles.buttonImg} src="/images/search.png" alt="" /> */}
+        <div className={styles.nav}>
+            <div className={styles.menu}>MOS</div>
+            <div className={styles.menu}>WORKS</div>
+            <div className={styles.menu}>CONTACT</div>
+        </div>
     </header>    
     );
 
