b5ce62631afa84705448b1a8055c341702550be7
kyondoku <kyondoku@gmail.com>
2022-08-31 02:14:54 +0900

    css적용성공 ㅠㅠ 스타일드컴포넌트짱

--- src/components/simple_slider/simple_slider.jsx
+++ src/components/simple_slider/simple_slider.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "./simple_slider.module.css";
+import styled from "styled-components";
 
 const SimpleSlider = () => {
     const settings = {
@@ -14,14 +15,31 @@ const SimpleSlider = () => {
         centerPadding: '20px',
       };
     
+    const Div = styled.div`
+      width: 100%;
+      height: 100%;
+      & div {
+        width: 30vw;
+        height: 30vw;
+      }
+    `;
+
     return (
         <div>
             <h2>Single Item</h2>
             <Slider {...settings}>
-                <div className={`${styles.frame} ${styles.pcria}`} />
-                <div className={`${styles.frame} ${styles.findyourperfume}`} />
-                <div className={`${styles.frame} ${styles.youtube}`} />
-                <div className={`${styles.frame} ${styles.dashboard}`} />
+                <Div>
+                    <div className={`${styles.frame} ${styles.pcria}`} />
+                </Div>
+                <Div>
+                    <div className={`${styles.frame} ${styles.findyourperfume}`} />
+                </Div>
+                <Div>
+                    <div className={`${styles.frame} ${styles.youtube}`} />
+                </Div>
+                <Div>
+                    <div className={`${styles.frame} ${styles.dashboard}`} />
+                </Div>
             </Slider>
         </div>
     )
