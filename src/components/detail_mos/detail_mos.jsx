8ea1da5af3302e32b1a78ce8a62f1d0637c4f0b8
kyondoku <kyondoku@gmail.com>
2022-10-05 01:19:25 +0900

    mos 미디어쿼리 적용 완료

--- src/components/detail_mos/detail_mos.jsx
+++ src/components/detail_mos/detail_mos.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import Draggable from "react-draggable";
+import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
 import styles from "./detail_mos.module.css";
 
 const DetailMos = () => {
+
+  const isPc = useMediaQuery({
+    query : "(min-width: 1024px)"
+  });
+
+  const isTablet = useMediaQuery({
+      query : "(min-width: 768px) and (max-width: 1023px)"
+  });
+
+  const isMobile = useMediaQuery({
+      query : "(max-width: 767px)"
+  });
 
   const [position, setPosition] = useState({ x: 0, y: 0 }); // box의 포지션 값
   // 업데이트 되는 값을 set 해줌
@@ -14,20 +27,56 @@ const DetailMos = () => {
   const nodeRef = React.useRef(null);
   
   return (
-    <div className={styles.detail}>
-      <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
-          <div ref={nodeRef} className={`${styles.skin} ${styles.skinKellogg}`}></div>
-      </Draggable>
-      <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
-          <div ref={nodeRef} className={`${styles.skin} ${styles.skinRubok}`}></div>
-      </Draggable>
-      <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
-          <div ref={nodeRef} className={`${styles.skin} ${styles.skinDavid}`}></div>
-      </Draggable>
-      <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
-          <div ref={nodeRef} className={`${styles.skin} ${styles.skinLeo}`}></div>
-      </Draggable>
-    </div>
+    <>
+    { isPc &&
+      <div className={styles.detail}>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.skin} ${styles.skinKellogg}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.skin} ${styles.skinRubok}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.skin} ${styles.skinDavid}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.skin} ${styles.skinLeo}`}></div>
+        </Draggable>
+      </div>
+    }
+    { isTablet &&
+      <div className={styles.taDetail}>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.taSkin} ${styles.skinKellogg}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.taSkin} ${styles.skinRubok}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.taSkin} ${styles.skinDavid}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.taSkin} ${styles.skinLeo}`}></div>
+        </Draggable>
+      </div>
+    }
+    { isMobile &&
+      <div className={styles.moDetail}>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.moSkin} ${styles.skinKellogg}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.moSkin} ${styles.skinRubok}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.moSkin} ${styles.skinDavid}`}></div>
+        </Draggable>
+        <Draggable onDrag={(e, data) => trackPos(data)}  nodeRef={nodeRef}>
+            <div ref={nodeRef} className={`${styles.moSkin} ${styles.skinLeo}`}></div>
+        </Draggable>
+      </div>
+    }
+    </>
   )
 };
 
