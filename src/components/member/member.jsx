021908658415fe65c2e3f11cbe664e91adb0b614
kyondoku <kyondoku@gmail.com>
2022-11-07 00:45:03 +0900

    meta태그 추가

--- src/components/member/member.jsx
+++ src/components/member/member.jsx
@@ -10,14 +10,21 @@ const Member = ({ member }) => {
         emailAddress,
         firstName,
         lastName,
-        imagePath,
+        profilePath,
         introduction,
         description
       } = member;
-      const url = imagePath || DEFAULT_IMAGE;
+      const url = profilePath || DEFAULT_IMAGE;
+      console.log(profilePath);
+
+      const templateStyle = {
+        backgroundImage: "url('../../../public/images/skins/intro_kellogg.PNG')"
+      };
+
       return (
         <div className={styles.memberBox}>       
           <img className={styles.profileImg} src={url} alt="profile photo" />
+          <div className={styles.test} style={templateStyle}>ㅇㄴㅇㄴㅇ</div>
           <div className={styles.info}>
             <h1 className={styles.nickName}>{nickName}</h1>
             <p className={styles.lastName}>{lastName}</p>
